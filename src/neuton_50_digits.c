6065df58b18d904bfec097a303f373f0fd22b780
Yuto Takano <moa17stock@gmail.com>
2024-10-30 18:56:31 +0100

    Swap Noto Serif for Pixellari font for legibility

--- src/neuton_50_digits.c
+++ src/neuton_50_digits.c
@@ -330,7 +330,7 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 #endif
 };
 
-extern const lv_font_t notoserif_18_ascii;
+extern const lv_font_t pixellari_20_ascii;
 
 
 /*-----------------
@@ -356,7 +356,7 @@ lv_font_t neuton_50_digits = {
 #endif
     .dsc = &font_dsc,          /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
 #if LV_VERSION_CHECK(8, 2, 0) || LVGL_VERSION_MAJOR >= 9
-    .fallback = &notoserif_18_ascii,
+    .fallback = &pixellari_20_ascii,
 #endif
     .user_data = NULL,
 };
